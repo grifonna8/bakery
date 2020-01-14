fdd56d904f4d36676f515c2a87d0be19bc8671f1
grifonna8 <grifonna8@gmail.com>
2020-01-14 17:32:14 +0300

    feat: added photos section

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
 $(document).ready(function () {
-  var form = $('.hero__form'),
-      formBtn = $('[data-toggle="form"]'),
-      closeBtn = $('.form__close');
+  var form = $('.modal'),
+      formBtn = $('[data-toggle="modal"]'),
+      closeBtn = $('.modal__close');
   
   formBtn.on('click', function () {
-    form.toggleClass('form--visible');
-    $('.form__close').addClass('form__close--visible');
-    $('.hero__form__dialogue').addClass('.hero__form__dialogue--visible');
-    
+    form.toggleClass('modal--visible');    
   });
   closeBtn.on('click', function () {
-    form.toggleClass('form--visible');
+    form.toggleClass('modal--visible');
   });
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
 });
